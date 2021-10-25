3b1b27bbd390862f564fe1a6dde04a4efac42d7d
Johanness15 <89245458+Johanness15@users.noreply.github.com>
2021-10-25 22:55:08 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
+from random import randint
+random_number = randint(1, 3)
+print("Jag tänker på ett tal mellan 1 och 3")
+guess = int(input("Gissa vilket tal jag tänker på!"))
+if guess == random_number:
+  print("Snyggt jobbat! Det var rätt!")
+else:
+  print("Tyvärr, det var fel.")
+print("Jag tänkte på " + str(random_number))
